1bcd077725cee619d627127504e1f42a89bf1d9f
Naim47300 <ragotnaim46@gmail.com>
2026-02-03 18:24:42 +0100

    Set API URL

--- dashboard/config.js
+++ dashboard/config.js
@@ -1,3 +1,3 @@
 // Config ComptaFlow (modifie ici l'URL de l'API une fois déployée)
 // Exemple: window.CF_API_BASE = "https://comptaflow-api.onrender.com";
-window.CF_API_BASE = "http://localhost:8000";
+window.CF_API_BASE = "https://comptaflow-r0k7.onrender.com";
